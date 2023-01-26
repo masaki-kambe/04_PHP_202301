e5d0c7cb9126105bcfaec5e20dfee125750e9081
kenshu <kenshu.app@gmail.com>
2023-01-26 17:56:43 +0900

    fix:09-6 score

--- 08/calcurator.php
+++ 08/calcurator.php
@@ -1,7 +1,11 @@
 <?php
-$num1 = $_POST['num1'];
-$num2 = $_POST['num2'];
-// $result = $num1 + $num2;
+$num1 = '';
+$num2 = '';
+if (!empty($_POST)) {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $result = $num1 + $num2;
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,10 +20,12 @@ $num2 = $_POST['num2'];
 
 <body>
     <h1>計算</h1>
-    <h2><?=$num1?> + <?=$num2?> = <?=$result?></h2>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+        <h2><?= $num1 ?> + <?= $num2 ?> = <?= $result ?></h2>
+    <?php endif; ?>
     <form action="" method="post">
-        <input type="text" name="num1" value="<?=htmlspecialchars($num1, ENT_QUOTES | ENT_HTML5, 'UTF-8')?>">+
-        <input type="text" name="num2" value="<?=$num2?>">=
+        <input type="text" name="num1" value="<?= htmlspecialchars($num1, ENT_QUOTES | ENT_HTML5, 'UTF-8') ?>">+
+        <input type="text" name="num2" value="<?= htmlspecialchars($num2, ENT_QUOTES | ENT_HTML5, 'UTF-8') ?>">=
         <p><input type="submit" value="計算"></p>
     </form>
 </body>
