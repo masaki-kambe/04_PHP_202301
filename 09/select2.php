3692c37c63b2d9227a2d10ee45e10d8afe7c9a57
kenshu <kenshu.app@gmail.com>
2023-01-26 19:02:15 +0900

    fix:09-9 switch

---
+++ 09/select2.php
@@ -0,0 +1,39 @@
+<?php
+// 初期値としてバナナを表示する
+$item = 'バナナ';
+if (!empty($_POST)) {
+    $item = $_POST['item'];
+}
+?>
+<html>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') : ?>
+        <p><?= $item ?>が選択されました。</p>
+    <?php endif; ?>
+    <form action="" method="post">
+        <p>
+            <select name="item">
+                <option <?php if ($item == 'リンゴ') echo 'selected'; ?>>リンゴ</option>
+                <option <?php if ($item == 'バナナ') echo 'selected'; ?>>バナナ</option>
+                <option <?php if ($item == 'ぶどう') echo 'selected'; ?>>ぶどう</option>
+                <option <?= $item == 'メロン' ? 'selected' : ''?>>メロン</option>
+                <option <?= $item == 'みかん' ? 'selected' : ''?>>みかん</option>
+                <option <?= $item == 'レモン' ? 'selected' : ''?>>レモン</option>
+            </select>
+        </p>
+        <p><input type="submit" value="送信"></p>
+    </form>
+</body>
+
+</html>
---
+++ 09/switch.php
@@ -0,0 +1,42 @@
+<?php
+// 初期値としてバナナを表示する
+$item = 'バナナ';
+if (!empty($_POST)) {
+    $item = $_POST['item'];
+}
+?>
+<html>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') : ?>
+        <p><?= $item ?>が選択されました。</p>
+    <?php endif; ?>
+    <form action="" method="post">
+        <p>
+            <select name="item">
+                <option <?php if ($item == 'リンゴ') echo 'selected'; ?>>リンゴ</option>
+                <option <?php if ($item == 'バナナ') echo 'selected'; ?>>バナナ</option>
+                <option <?php if ($item == 'ぶどう') echo 'selected'; ?>>ぶどう</option>
+                <option <?= $item == 'メロン' ? 'selected' : ''?>>メロン</option>
+                <option <?= $item == 'みかん' ? 'selected' : ''?>>みかん</option>
+                <option <?= $item == 'レモン' ? 'selected' : ''?>>レモン</option>
+                <option <?php switch ($item) {case 'キウイ': echo 'selected';}?>>キウイ</option>
+                <option <?php switch ($item) {case 'いちご': echo 'selected';}?>>いちご</option>
+                <option <?php switch ($item) {case 'スイカ': echo 'selected';}?>>スイカ</option>
+            </select>
+        </p>
+        <p><input type="submit" value="送信"></p>
+    </form>
+</body>
+
+</html>
