e83630217afefe8305026b10d3d52e423b72c937
kenshu <kenshu.app@gmail.com>
2023-01-26 12:28:32 +0900

    fix:08 cart

--- 08/calcurator.php
+++ 08/calcurator.php
@@ -1,7 +1,7 @@
 <?php
 $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
-$result = $num1 + $num2;
+// $result = $num1 + $num2;
 ?>
 
 <!DOCTYPE html>
@@ -18,8 +18,8 @@ $result = $num1 + $num2;
     <h1>計算</h1>
     <h2><?=$num1?> + <?=$num2?> = <?=$result?></h2>
     <form action="" method="post">
-        <input type="text" name="num1" size="2" maxlength="2" value="<?=$num1?>">+
-        <input type="text" name="num2" size="2" maxlength="2" value="<?=$num2?>">=
+        <input type="text" name="num1" value="<?=htmlspecialchars($num1, ENT_QUOTES | ENT_HTML5, 'UTF-8')?>">+
+        <input type="text" name="num2" value="<?=$num2?>">=
         <p><input type="submit" value="計算"></p>
     </form>
 </body>
---
+++ 08/cart1.php
@@ -0,0 +1,56 @@
+<?php
+$goods['name']  = '和風柄レターセット';
+$goods['price'] = 980;
+
+$count = $_POST['count'];
+$totalPrice = $count * $goods['price'];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ショッピングカート</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 600px;
+        }
+
+        th,
+        td {
+            border: 1px solid #666666;
+            padding: 4px;
+        }
+
+        th {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>ショッピングカート</h1>
+    <form action="" method="post" novalidate>
+        <table>
+            <tr>
+                <th>商品名</th>
+                <th>単価</th>
+                <th>数量</th>
+                <th>小計</th>
+            </tr>
+            <tr>
+                <td><?=$goods['name']?></td>
+                <td><?=$goods['price']?>円</td>
+                <td><input type="text" name="count" value="<?=htmlspecialchars($count, ENT_QUOTES | ENT_HTML5, 'UTF-8')?>"></td>
+                <td><?=$totalPrice?>円</td>
+            </tr>
+        </table>
+        <p><input type="submit" value="送信"></p>
+    </form>
+</body>
+
+</html>
