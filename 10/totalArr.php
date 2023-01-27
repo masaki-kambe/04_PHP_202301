f76cc11769b041e8950b7dd3682b41fb3d1188bf
kenshu <kenshu.app@gmail.com>
2023-01-27 12:42:33 +0900

    fix:10-5 totalArr

---
+++ 10/totalArr.php
@@ -0,0 +1,11 @@
+<?php
+
+$priceList = [298, 348, 198, 680, 98, 980, 498, 640];
+
+$total = 0;
+
+for ($i = 0; $i < count($priceList); $i++) {
+    $total += $priceList[$i];
+}
+
+echo $total * 1.1 . '円';
