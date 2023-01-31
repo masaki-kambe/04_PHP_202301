36e4c79ffb9ef109fa1d88d7724626e9399934eb
kenshu <kenshu.app@gmail.com>
2023-01-31 12:16:51 +0900

    fix:11-8 di

--- 11/di.php
+++ 11/di.php
@@ -2,7 +2,7 @@
 /**
  * 不快指数を計算する
 */
-$t = 13;   // 気温 T
+$t = 20;   // 気温 T
 $h = 40;   // 湿度 H
 
 
@@ -13,11 +13,29 @@ $h = 40;   // 湿度 H
  * @param integer|null $h
  * @return float|null
  */
-function getDi(?int $t, ?int $h): ?float
+function getDi(mixed $t = null, mixed $h): bool | array
 {
-    if (empty($t) || empty($h)) return null;
-    return 0.81 * $t + 0.01 * $h * (0.99 * $t - 14.3) + 46.3;
+    if (is_null($t) || $t === '' || empty($h)) return false;
+    $diArr['di'] = 0.81 * $t + 0.01 * $h * (0.99 * $t - 14.3) + 46.3;
+    if ($diArr['di'] < 55) {
+        $diArr['result'] = '寒い';
+    } elseif ($diArr['di'] < 60) {
+        $diArr['result'] = '肌寒い';
+    } elseif ($diArr['di'] < 65) {
+        $diArr['result'] = '何も感じない';
+    } elseif ($diArr['di'] < 70) {
+        $diArr['result'] = '快い';
+    } elseif ($diArr['di'] < 75) {
+        $diArr['result'] = '暑くない';
+    } elseif ($diArr['di'] < 80) {
+        $diArr['result'] = 'やや暑い';
+    } elseif ($diArr['di'] < 85) {
+        $diArr['result'] = '暑くて汗が出る';
+    } else {
+        $diArr['result'] = '暑くてたまらない';
+    }
+    return $diArr;
 }
 ?>
 
-気温<?=$t?>℃、湿度<?=$h?>%の時の不快指数は<?=getDi($t, $h)?>です。
+気温<?=$t?>℃、湿度<?=$h?>%の時の不快指数は<?=getDi($t, $h)['di']?>で「<?=getDi($t, $h)['result']?>」です。
